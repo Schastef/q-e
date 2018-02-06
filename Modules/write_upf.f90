d5cc7e9c9075c476cd2c370165a43e4b2f649e8e
Pietro Delugas <pdelugas@sissa.it>
2018-02-06 18:52:21 +0100

    small program assed to fix non xml compliant UPFs
    Added a small program to fix non xml compliant UPF files,
    added in Modules/write_upf.f90 a driver routine for writing
    xml files using FoX avoiding encapsulating the usage of FoX libs.

--- Modules/write_upf.f90
+++ Modules/write_upf.f90
@@ -21,6 +21,43 @@ MODULE  write_upf_module
    
    USE write_upf_v2_module, ONLY: write_upf_v2
    USE write_upf_schema_module, ONLY: write_upf_schema
-   PUBLIC
+   PUBLIC 
+CONTAINS 
+   SUBROUTINE write_upf(filename, upf, schema , conf, u_input) 
+      !! this routine writes an upf file with name = filename using 
+      !! data contained in pseudo_upf structure upf 
+      !! if schema = 'qe_pp' the new qe_pp schema the old upf v2.1 is used 
+      !! otherwise
+      USE FoX_wxml 
+      USE pseudo_types, ONLY: pseudo_upf, pseudo_config
+      IMPLICIT NONE
+      CHARACTER(LEN=*),INTENT(IN)   :: filename
+      !! name of the output file
+      TYPE(pseudo_upf),INTENT(IN)   :: upf
+      !! pseudo_upf structure containing all the pseudo data
+      CHARACTER(LEN=*),OPTIONAL,INTENT(IN) :: schema
+      !!  optional, character flag which selects what schema will be used on writing 
+      TYPE(pseudo_config),OPTIONAL,INTENT(IN)   :: conf 
+      !!  optional, pseudo_conf data structure containing the atomic configuration used
+      !!  to generate the pseudo
+      INTEGER,OPTIONAL                        :: u_input
+      !!  optional: unit of stdin for  the generation program, used to write the 
+      !!  generation input in the upf file
+      TYPE(xmlf_t)                   :: xf_desc
+      CHARACTER(LEN=10)              :: schema_
+      !
+      CALL  xml_OpenFile (filename = TRIM(filename), XF = xf_desc, UNIT = 2, PRETTY_PRINT =.true., &
+                                 REPLACE = .true., NAMESPACE = .true. )
+      IF ( PRESENT(schema )) schema_ = TRIM(schema) 
+      SELECT CASE (TRIM(schema_))
+         CASE ('qe_pp', 'QE_PP') 
+            CALL write_upf_schema(xf_desc, upf = upf, CONF = conf, U_INPUT = u_input) 
+         CASE ('V2', 'v2' ,'upf', 'UPF') 
+            CALL write_upf_v2(xf_desc,upf=upf, CONF = conf, U_INPUT = u_input)
+      END SELECT
+
+      CALL xml_Close(xf_desc)
+   END SUBROUTINE write_upf
+
 END MODULE write_upf_module
 
