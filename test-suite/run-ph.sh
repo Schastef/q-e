91439bc288dfafbfd8f200822877a955e6c64362
mach3da <macheda.franc@gmail.com>
2025-01-14 22:22:23 +0100

    Adding pip3 installation

--- test-suite/run-ph.sh
+++ test-suite/run-ph.sh
@@ -119,6 +119,7 @@ then
   echo "Before running the multipole.py Python script, pip install spglib dependency"
   echo " "
   pip install spglib
+  pip3 install spglib
   echo " "
   echo "Running Python multipole.py preprocessing..."
   python3 multipole.py -e --order 3 --epsil_order 4 -p -n 8 --mesh 2 2 2 --mesh_step 0.01 > preprocessing.out
