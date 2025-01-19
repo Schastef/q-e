69161fef639074dd48d62043ad8ca86de20ce527
mach3da <macheda.franc@gmail.com>
2025-01-19 19:10:15 +0100

    Setting a looser threshold and making the test more resilient

--- test-suite/run-ph.sh
+++ test-suite/run-ph.sh
@@ -126,7 +126,7 @@ then
   pip3 install spglib
   echo " "
   echo "Running Python multipole.py preprocessing..."
-  python3 multipole.py -e --order 3 --epsil_order 4 -p -n 8 --mesh 2 2 2 --mesh_step 0.01 > preprocessing.out
+  python3 multipole.py -e --order 3 --epsil_order 4 -p -n 8 --mesh 2 2 2 --mesh_step 0.1 > preprocessing.out
 elif [[ "$1" == "13" ]]
 then
   echo "Running PH ..."
